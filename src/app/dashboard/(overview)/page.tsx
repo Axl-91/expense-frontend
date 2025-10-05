220d76018f2b15882ea55150282af77c61c12ca9
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-10-05 18:38:10 -0300

    Minor refactors on the dashboard home page

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,21 +1,35 @@
 import { getUser } from "@/lib/api/users.server";
+import Link from "next/link";
 
 async function DashboardPage() {
   const user = await getUser();
 
   return (
-    <>
+    <div className="flex flex-1 flex-col h-full">
       <header className="mb-6">
         <h1 className="text-2xl font-semibold tracking-tight border-b border-slate-700 pb-2">
           Welcome back, <span className="text-indigo-400">{user.username}</span>
         </h1>
       </header>
 
-      <section>
-        <h2 className="text-lg font-medium mb-3 text-indigo-300">Reports</h2>
-        <p> This is where I should recollect data from reports of the users and show it </p>
-      </section>
-    </>
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <strong className="text-white text-3xl md:text-4xl mb-5">Dashboard messages</strong>
+        <div className="flex flex-col items-center justify-center sm:flex-row gap-4">
+          <Link
+            href="/dashboard/reports"
+            className="root-button rounded-lg bg-indigo-600 text-white hover:bg-indigo-700"
+          >
+            Reports
+          </Link>
+          <Link
+            href="#"
+            className="root-button rounded-lg bg-green-600 text-white hover:bg-green-700"
+          >
+            Create new Report
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
